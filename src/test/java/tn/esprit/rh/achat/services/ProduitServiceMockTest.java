5f7851720d42d87fdeefa6cc0b02ec50be906897
raniabenmohamed <79701380+raniabenmohamed@users.noreply.github.com>
2022-11-17 18:15:44 +0100

    Update ProduitServiceMockTest.java

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceMockTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceMockTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+package tn.esprit.rh.achat.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
